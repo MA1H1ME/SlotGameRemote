7f750012fef24a53f687c26aece9a651a1b6fd9e
MA1H1ME <horiguchishotaro@gmail.com>
2021-10-26 12:28:34 +0900

    リール回転完成+配列化+ボタン描画

--- SlotGameTest/GameScene.cpp
+++ SlotGameTest/GameScene.cpp
@@ -1,49 +1,62 @@
 ﻿#include"GameScene.h"
 
-GameScene::GameScene(const InitData& init)
+GameScene::GameScene(const InitData& init)//スコア引継ぎなどの数値
 	:IScene(init)
 {
 
 
 }
+//アップデート関数(Manager管理)---------------------------------------
 void GameScene::update()  {
+	
+	ReelGenetrate();
 
 	
-	Txt_Reel1A.draw(Reel1A_NowPos);
-	Txt_Reel2A.draw(Reel2A_NowPos);
-	Txt_Reel3A.draw(Reel3A_NowPos);
-	Txt_Reel1B.draw(Reel1B_NowPos);
-	Txt_Reel2B.draw(Reel2B_NowPos);
-	Txt_Reel3B.draw(Reel3B_NowPos);
-
-
-	Reel1A_NowPos += speed;
-	Reel1B_NowPos += speed;
-
-	if (Reel1A_NowPos.y>Reel_MaxPos)
-	{
-		Reel1A_NowPos.y = Reel1B_NowPos.y - 720;
-	}
-	if (Reel1B_NowPos.y > Reel_MaxPos)
-	{
-		Reel1B_NowPos.y = Reel1A_NowPos.y - 720;
-	}
 	
-	/*Print << Reel1A_NowPos;
-	Print << Reel1B_NowPos;*/
-		
-		
+	if (MouseL.down())
+	{
+		Cursor::Pos();
+	}
 }
+//リール回転関数---------------------------------------
+void GameScene::ReelGenetrate() {
+	//描画
+	for (int i = 0; i <= 5; i++)
+	{
+		Reel_Tex[i].draw(Reel_NowPos[i]);
+		Reel_NowPos[i] += speed;
+	}
+	//リールを入れ替える
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 3; j <= 5; j++)
+		{
+			if (Reel_NowPos[i].y >= Reel_MaxPos)
+			{
+				Reel_NowPos[i].y = Reel_NowPos[j].y - 720;
+			}
+			if (Reel_NowPos[j].y >= Reel_MaxPos)
+			{
+				Reel_NowPos[j].y = Reel_NowPos[i].y - 720;
+			}
+		}
+	}
+#pragma region リール回転デバッグ用
+
+	Print << Reel_NowPos[0];
+
+#pragma endregion
+}
+//描画関数(Manager管理)---------------------------------------
 void GameScene::draw() const  
 {	
 	txt_Dai.resized(890).draw(180, 0);
-	txt_StopButton1.resized(80).draw(405, 300);
+	Circle{ 440,380,40 }.draw(Palette::Red);
+	Circle{ 620,380,40 }.draw(Palette::Red);
+	Circle{ 800,380,40 }.draw(Palette::Red);
+	/*txt_StopButton1.resized(80).draw(405, 300);
 	txt_StopButton2.resized(80).draw(585, 300);
-	txt_StopButton3.resized(80).draw(765, 300);
-
-
-	
+	txt_StopButton3.resized(80).draw(765, 300);*/
 }
-
 
 
--- SlotGameTest/GameScene.h
+++ SlotGameTest/GameScene.h
@@ -2,67 +2,69 @@
 # include "Common.hpp"
 class GameScene : public MyApp::Scene {
 private:
-
+#pragma region 画像読み込み
 	//本体Texture
 	const Texture txt_Dai{ U"example/texture/Sotowaku.png" };
 	const Texture txt_StopButton1{ U"example/texture/StopButton.png" };
 	const Texture txt_StopButton2{ U"example/texture/StopButton.png" };
 	const Texture txt_StopButton3{ U"example/texture/StopButton.png" };
 
+	
 	//リールTexture
-    const Texture Txt_Reel1A{U"example/Reel/Reel.png"};
-	const Texture Txt_Reel2A{ U"example/Reel/Reel.png" };
-	const Texture Txt_Reel3A{ U"example/Reel/Reel.png" };
-	const Texture Txt_Reel1B{ U"example/Reel/Reel.png" };
-	const Texture Txt_Reel2B{ U"example/Reel/Reel.png" };
-	const Texture Txt_Reel3B{ U"example/Reel/Reel.png" };
-	int count = 0;
+	Array<Texture>Reel_Tex = {
+		Texture {U"example/Reel/Reel.png"},
+		Texture {U"example/Reel/Reel.png"},
+		Texture {U"example/Reel/Reel2.png"},
+		Texture {U"example/Reel/Reel2.png"},
+		Texture {U"example/Reel/Reel3.png"},
+		Texture {U"example/Reel/Reel3.png"},
+	};
+#pragma endregion
 
+
+	
 	//リールの初期位置
-	const int reel1A_initialPos = -440;
-	const int reel2A_initialPos = -360;
-	const int reel3A_initialPos =-40 ;
-
-	const int reel1B_initialPos = -440+(-720);
-	const int reel2B_initialPos = -360 + (-720);
-	const int reel3B_initialPos = -40 + (-720);
-
-	
+	Array<int> reel_initialPos = {
+		-440,
+		-440 + (-720)
+	};
 	//リールの回転速度
-	Vec2 speed = Vec2(0.0, 20.0);
-
-	 Vec2 Reel1A_NowPos =  Vec2(370, reel1A_initialPos);
-	 Vec2 Reel2A_NowPos = Vec2(550, reel2A_initialPos);
-	 Vec2 Reel3A_NowPos = Vec2(730,reel3A_initialPos) ;
-
-
-	 Vec2 Reel1B_NowPos = Vec2(370, reel1B_initialPos);
-	 Vec2 Reel2B_NowPos = Vec2(550, reel2B_initialPos);
-	 Vec2 Reel3B_NowPos = Vec2(730, reel3B_initialPos);
-
-
-	 //リールの最大値
-	 const int Reel_MaxPos = 280;
-	 
+	Vec2 speed = Vec2(0.0, 32.0);
+	//リールの今の場所
+	Array<Vec2>Reel_NowPos = {
+		Vec2{370, reel_initialPos[0] },
+		Vec2{550, reel_initialPos[0] },
+		Vec2{730, reel_initialPos[0] },
+		Vec2{370, reel_initialPos[1] },
+		Vec2{550, reel_initialPos[1] },
+		Vec2{730, reel_initialPos[1] },
+	};
+	//リールの最大値
+	const int Reel_MaxPos = 280;
+	//描画系
+	double delayTime = 0.0;
 	
 
+	//------------------------------------------------
 	bool stopflag1 = false;//ボタンが押されたかどうか
-    bool stopflag2 = false;
+	bool stopflag2 = false;
 	bool stopflag3 = false;
 	bool allflag = true;
 
-	float delayTime = 0;
-	const float FrameTime = 1.0f / 60.0f;
+
+//---------------------------------------------------------------
 public:
 
 	GameScene(const InitData& init);//シーン管理
 	void update() override;
 	void draw() const override;
-
+	void ReelGenetrate();//リール回転
 
 	
 };
-/*
+
+
+/*Memo
 約絵柄サイズ=80
 リールPos＝-440,-360,-280,-200,-120,-40,40,
 */
